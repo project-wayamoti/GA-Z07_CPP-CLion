6fadbc9ced34d144fda362d6de051699bed732d0
waya <42435123+waya0125@users.noreply.github.com>
2023-06-16 15:32:36 +0900

    2つ同時にたまが動き、自機が独立して操作できるように仕様変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 // 以下の行がないとコンパイルが通りません
 #pragma comment(linker, "/subsystem:windows")
-
 #include "DxLib.h"
 #include "option.h"
 #define WIDTH 1280
@@ -41,16 +40,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     //## 環境に応じて設定
     // 移動速度
-    int moveSpeed = 4; // 60Hz=8, 300Hz=1
+    int moveSpeed = 1; // 60Hz=8, 300Hz=1
 
     //## 固有の設定
     // 色の設定
-    unsigned int mainColor = GetColor(0, 0, 255);
-    unsigned int subColor = GetColor(255, 0, 0);
+    unsigned int charaColor = GetColor(255, 255, 255);
+    unsigned int enemy1Color = GetColor(255, 0, 0);
+    unsigned int enemy2Color = GetColor(0, 255, 0);
 
     // Vector2構造体の宣言
-    // pos: 現在の座標, move: 移動量, axis: 軸, goal: 目標座標, moveVec: 移動ベクトル
-    Vector2 pos, move, axis, goal, moveVec;
+    // pos: 現在の座標, move: 移動量, moveVec: 移動ベクトル
+    Vector2 pos, move, moveVec, enemy1, enemy2, enemy1Vec, enemy2Vec;
 
     // 移動物体初期設定
     pos.x = WIDTH / 2;  // 画面の中心に設定
@@ -61,104 +61,61 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     moveVec.x = 1;      // 移動ベクトル
     moveVec.y = 0;
 
-    // 回転軸の座標設定
-    axis.x = 500;
-    axis.y = 370;
-    axis.size = 8;      // 回転軸のサイズ
-    int safeArea = 10;  // 範囲許容値
-
-    // 目標座標設定
-    goal.x = 1000;
-    goal.y = 700;
-
-    // 回転軸複数設定 ランダムな値を設定する
-    // 参考文献 : http://www7b.biglobe.ne.jp/~robe/cpphtml/html01/cpp01062.html
-    float axiss[3][2] = {
-            {
-                    static_cast<float>(getRandom(150, 450)), static_cast<float>(getRandom(150, HEIGHT - 150))
-            },
-            {
-                    static_cast<float>(getRandom(550, 850)), static_cast<float>(getRandom(150, HEIGHT - 150))
-            },
-            {
-                    static_cast<float>(getRandom(950, 1250)), static_cast<float>(getRandom(150, HEIGHT - 150))
-            }
-    };
-
-    //##### ここまで初期設定 #####//
+    // 敵物体の初期設定
+    enemy1.x = 100;
+    enemy1.y = 100;
+    enemy1.size = 32;
+    enemy1Vec.x = 1.0f;
+    enemy1Vec.y = 0.15f;
+    enemy2.x = 800;
+    enemy2.y = 600;
+    enemy2.size = 32;
+    enemy2Vec.x = 0.15f;
+    enemy2Vec.y = 1.0f;
 
     //##### メインループ（描画処理） #####//
     while (ProcessMessage() == 0) {
         // 画面の更新
         ClearDrawScreen();
 
-        //##### 回転可能範囲 (円)
-        // 計算式: (軸のサイズ * 10) + (移動物体のサイズ)
-        // TRUE 塗りつぶし / FALSE 塗りつぶさない
-        if (getDistance(pos.x, pos.y, axis.x, axis.y) < axis.size * safeArea + pos.size) {
-            DrawCircleAA(500.0f, 370.0f, axis.size, 16, GetColor(255, 255, 255), TRUE);
-            DrawLine(pos.x, pos.y, axis.x, axis.y, GetColor(255, 255, 255));
-
-            // マウス左クリックされた際、かつ回転可能範囲内である場合
-            if (GetMouseInput() & MOUSE_INPUT_LEFT && getDistance(pos.x, pos.y, axis.x, axis.y) < axis.size * safeArea + pos.size) {
-                // 関数叩いて進行方向ベクトルを更新する
-                moveVec = findDistance(pos.x, pos.y, axis.x, axis.y);
-            }
-        }
-        else {
-            DrawCircleAA(500.0f, 370.0f, axis.size, 16, GetColor(255, 255, 255), FALSE);
-        }
-        DrawCircleAA(500.0f, 370.0f, axis.size * safeArea, 16, GetColor(255, 255, 255), FALSE);
-
-        //##### 複数軸の描画（脳筋実装版）
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 2; j++) {
-                if (getDistance(pos.x, pos.y, axiss[i][0], axiss[i][1]) < axis.size * safeArea + pos.size) {
-                    DrawCircleAA(axiss[i][0], axiss[i][1], axis.size, 16, GetColor(255, 255, 255), TRUE);
-                    DrawLine(pos.x, pos.y, axiss[i][0], axiss[i][1], GetColor(255, 255, 255));
-                }
-                else {
-                    DrawCircleAA(axiss[i][0], axiss[i][1], axis.size, 16, GetColor(255, 255, 255), FALSE);
-                }
-                DrawCircleAA(axiss[i][0], axiss[i][1], axis.size * safeArea, 16, GetColor(255, 255, 255), FALSE);
-
-                // マウス左クリックされた際、かつ回転可能範囲内である場合
-                if (GetMouseInput() & MOUSE_INPUT_LEFT && getDistance(pos.x, pos.y, axiss[i][0], axiss[i][1]) < axis.size * safeArea + pos.size) {
-                    // 関数叩いて進行方向ベクトルを更新する
-                    moveVec = findDistance(pos.x, pos.y, axiss[i][0], axiss[i][1]);
-                }
-            }
-        }
-
-        //##### ここから 中央物体の処理
-
+        //##### 中央物体の処理
         //球体移動処理 TRUE 塗りつぶし / FALSE 塗りつぶさない
-        DrawCircle(pos.x, pos.y, pos.size, mainColor, TRUE);
+        DrawCircle(pos.x, pos.y, pos.size, charaColor, TRUE);
+        // WASDキーでposを移動させる
+        if (CheckHitKey(KEY_INPUT_W)) {
+            pos.y -= move.y;
+        }
+        if (CheckHitKey(KEY_INPUT_S)) {
+            pos.y += move.y;
+        }
+        if (CheckHitKey(KEY_INPUT_A)) {
+            pos.x -= move.x;
+        }
+        if (CheckHitKey(KEY_INPUT_D)) {
+            pos.x += move.x;
+        }
+
+        //##### 敵物体の処理
+
+        // 敵物体移動処理
+        DrawCircle(enemy1.x, enemy1.y, enemy1.size, enemy1Color, TRUE);
+        DrawCircle(enemy2.x, enemy2.y, enemy2.size, enemy2Color, TRUE);
 
         //移動計算
-        pos.x = pos.x + moveVec.x * move.x;
-        pos.y = pos.y + moveVec.y * move.y;
+        enemy1.x = enemy1.x + enemy1Vec.x * moveSpeed;
+        enemy1.y = enemy1.y + enemy1Vec.y * moveSpeed;
+        enemy2.x = enemy2.x + enemy2Vec.x * moveSpeed;
+        enemy2.y = enemy2.y + enemy2Vec.y * moveSpeed;
 
         //壁に当たったら反射
-        if (pos.x < 0 + pos.size || pos.x > WIDTH - pos.size)  moveVec.x = -moveVec.x;
-        if (pos.y < 0 + pos.size || pos.y > HEIGHT - pos.size) moveVec.y = -moveVec.y;
+        if (enemy1.x < 0 + enemy1.size || enemy1.x > WIDTH - enemy1.size)  enemy1Vec.x = -enemy1Vec.x;
+        if (enemy1.y < 0 + enemy1.size || enemy1.y > HEIGHT - enemy1.size) enemy1Vec.y = -enemy1Vec.y;
+        if (enemy2.x < 0 + enemy2.size || enemy2.x > WIDTH - enemy2.size)  enemy2Vec.x = -enemy2Vec.x;
+        if (enemy2.y < 0 + enemy2.size || enemy2.y > HEIGHT - enemy2.size) enemy2Vec.y = -enemy2Vec.y;
 
-        //##### ここから 目標座標の処理
-        // 目標座標の描画
-        DrawBox(goal.x, goal.y, goal.x + pos.size, goal.y + pos.size, GetColor(255, 255, 255), FALSE);
-
-        // 目標座標に到達したら新しい目標座標を設定する
-        if (getDistance(pos.x, pos.y, goal.x, goal.y) < (pos.size * 2)) {
-            goal.x = getRandom(0 + pos.size, WIDTH - pos.size);
-            goal.y = getRandom(0 + pos.size, HEIGHT - pos.size);
-        }
-
-        //##### ここから 描画処理
+        //##### 描画処理
         /* デバッグ用
-        printfDx("x:%.2f y:%.2f\n", pos.x, pos.y);
-        printfDx("x1:%.2f y1:%.2f\n", axis.x, axis.y);
-        printfDx("moveVector x:%.2f y:%.2f\n", moveVec.x, moveVec.y);
-        printfDx("dis:%f\n", getDistance(pos.x, pos.y, axis.x, axis.y));
+        printfDx();
         */
 
         // 画面の更新（必須）
